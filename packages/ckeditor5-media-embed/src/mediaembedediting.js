9f37dc3816377d2b22f5ee0c4f6666823c551859
Suvan Ramani <suvanramani@gmail.com>
2021-11-03 10:59:43 -0700

    add id to iframe

--- packages/ckeditor5-media-embed/src/mediaembedediting.js
+++ packages/ckeditor5-media-embed/src/mediaembedediting.js
@@ -194,7 +194,7 @@ export default class MediaEmbedEditing extends Plugin {
 
 						return (
 							'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
-								`<iframe src="https://www.loom.com/embed/${ id }" ` +
+								`<iframe id="fjvideoembed-${ id }" src="https://www.loom.com/embed/${ id }" ` +
 									'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
 									'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
 								'</iframe>' +
