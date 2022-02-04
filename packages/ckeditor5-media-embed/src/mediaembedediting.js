fef9c0de28bfe06532ce9cda5a7ddc255b20731c
suvan92 <suvanramani@gmail.com>
2022-02-03 16:17:59 -0800

    update gong regex (#11)

--- packages/ckeditor5-media-embed/src/mediaembedediting.js
+++ packages/ckeditor5-media-embed/src/mediaembedediting.js
@@ -226,15 +226,15 @@ export default class MediaEmbedEditing extends Plugin {
 				{
 					name: 'Gong',
 					url: [
-						/^app\.gong\.io\/call\?id=([\w-]+)/,
-						/^app\.gong\.io\/embedded-call\?call-id=([\w-]+)/
+						/^app\.gong\.io\/call\?id=(.+)/,
+						/^app\.gong\.io\/embedded-call\?call-id=(.+)/
 					],
 					html: match => {
-						const id = match[ 1 ];
+						const idAndParams = match[ 1 ];
 
 						return (
 							'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
-								`<iframe src="https://app.gong.io/embedded-call?call-id=${ id }" ` +
+								`<iframe src="https://app.gong.io/embedded-call?call-id=${ idAndParams }" ` +
 									'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
 									'frameborder="0">' +
 								'</iframe>' +
